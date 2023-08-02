bb5891653dd7bd7ad21dcc82f71ec43943fc9cd6
Arnau Mora <arnyminerz@proton.me>
2023-08-02 18:47:54 +0200

    Fixed test
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/test/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/utils/ConditionUtilsTest.kt
+++ src/test/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/utils/ConditionUtilsTest.kt
@@ -7,14 +7,14 @@ import kotlin.test.assertTrue
 class ConditionUtilsTest {
     @Test
     fun `test areAllNull`() {
-        assertTrue(areAllNull())
+        assertTrue(areAllNull<Void>())
         assertTrue(areAllNull(null, null, null))
         assertFalse(areAllNull(true, null, null))
     }
 
     @Test
     fun `test isAnyNull`() {
-        assertFalse(isAnyNull())
+        assertFalse(isAnyNull<Void>())
         assertTrue(isAnyNull(null, null, null))
         assertTrue(isAnyNull(true, true, null))
         assertFalse(isAnyNull(true, true, false))
