38bf32d978e0627014f1e98f0b3c3f8e952c79ac
Arnau Mora <arnyminerz@proton.me>
2023-08-02 18:14:45 +0200

    Added colors to logger
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/Logger.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/Logger.kt
@@ -1,9 +1,11 @@
 package com.arnyminerz.escalaralcoiaicomtat.backend
 
+import com.arnyminerz.escalaralcoiaicomtat.backend.utils.AnsiColors
+
 object Logger {
-    fun info(message: String) = println(message)
+    fun info(message: String) = println(AnsiColors.BLUE + message + AnsiColors.RESET)
 
-    fun warn(message: String) = println(message)
+    fun warn(message: String) = println(AnsiColors.YELLOW + message + AnsiColors.RESET)
 
-    fun error(message: String) = println(message)
+    fun error(message: String) = println(AnsiColors.YELLOW + message + AnsiColors.RESET)
 }
