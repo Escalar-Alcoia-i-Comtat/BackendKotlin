df6ee206842dba6507ec389e954adce52553592e
Arnau Mora <arnyminerz@proton.me>
2023-08-05 17:25:27 +0200

    Improved grades processing
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/data/Grade.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/data/Grade.kt
@@ -2,10 +2,14 @@ package com.arnyminerz.escalaralcoiaicomtat.backend.data
 
 interface GradeValue {
     companion object {
-        fun fromString(value: String): GradeValue? {
-            val name = value.replace("+", "_PLUS")
+        fun fromString(value: String): GradeValue {
+            val name = value
+                .uppercase()
+                .replace("+", "_PLUS")
+                .replace("º", "A")
             return SportsGrade.entries.find { it.name.endsWith(name) }
                 ?: ArtificialGrade.entries.find { it.name == name }
+                ?: SportsGrade.UNKNOWN
         }
     }
 
@@ -17,11 +21,12 @@ enum class SportsGrade : GradeValue {
     G2, G2_PLUS,
     G3A, G3B, G3C,
     G4A, G4B, G4C,
-    G5A, G5B, G5C,
+    G5A, G5B, G5C, G5_PLUS,
     G6A, G6A_PLUS, G6B, G6B_PLUS, G6C, G6C_PLUS,
     G7A, G7A_PLUS, G7B, G7B_PLUS, G7C, G7C_PLUS,
     G8A, G8A_PLUS, G8B, G8B_PLUS, G8C, G8C_PLUS,
-    G9A, G9A_PLUS, G9B, G9B_PLUS, G9C, G9C_PLUS
+    G9A, G9A_PLUS, G9B, G9B_PLUS, G9C, G9C_PLUS,
+    UNKNOWN
 }
 
 enum class ArtificialGrade : GradeValue {
---
+++ src/test/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/data/TestGrades.kt
@@ -0,0 +1,17 @@
+package com.arnyminerz.escalaralcoiaicomtat.backend.data
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestGrades {
+    @Test
+    fun `Test grade conversions`() {
+        assertEquals(SportsGrade.G4A, GradeValue.fromString("4º"))
+        assertEquals(SportsGrade.G5A, GradeValue.fromString("5º"))
+        assertEquals(SportsGrade.G5_PLUS, GradeValue.fromString("5+"))
+        assertEquals(SportsGrade.G6A, GradeValue.fromString("6a"))
+        assertEquals(SportsGrade.G6B_PLUS, GradeValue.fromString("6b+"))
+        assertEquals(SportsGrade.G7C, GradeValue.fromString("7c"))
+        assertEquals(SportsGrade.G9C_PLUS, GradeValue.fromString("9c+"))
+    }
+}
