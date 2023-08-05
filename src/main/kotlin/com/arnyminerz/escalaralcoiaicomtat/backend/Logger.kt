e9d0b4e4212c21c416b8b8102a668faa887e594f
Arnau Mora <arnyminerz@proton.me>
2023-08-05 18:49:59 +0200

    Added log levels
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/Logger.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/Logger.kt
@@ -3,11 +3,31 @@ package com.arnyminerz.escalaralcoiaicomtat.backend
 import com.arnyminerz.escalaralcoiaicomtat.backend.utils.AnsiColors
 
 object Logger {
-    fun debug(message: String) = println(AnsiColors.WHITE + message + AnsiColors.RESET)
+    @LogLevel
+    var level = LogLevel.INFO
 
-    fun info(message: String) = println(AnsiColors.BLUE + message + AnsiColors.RESET)
+    fun debug(message: String) {
+        if (level >= LogLevel.DEBUG) println(AnsiColors.WHITE + message + AnsiColors.RESET)
+    }
 
-    fun warn(message: String) = println(AnsiColors.YELLOW + message + AnsiColors.RESET)
+    fun info(message: String) {
+        if (level >= LogLevel.INFO) println(AnsiColors.BLUE + message + AnsiColors.RESET)
+    }
 
-    fun error(message: String) = println(AnsiColors.YELLOW + message + AnsiColors.RESET)
+    fun warn(message: String) {
+        if (level >= LogLevel.WARN) println(AnsiColors.YELLOW + message + AnsiColors.RESET)
+    }
+
+    fun error(message: String) {
+        if (level >= LogLevel.ERROR) println(AnsiColors.YELLOW + message + AnsiColors.RESET)
+    }
+}
+
+annotation class LogLevel {
+    companion object {
+        const val DEBUG = 0
+        const val INFO = 1
+        const val WARN = 2
+        const val ERROR = 3
+    }
 }
