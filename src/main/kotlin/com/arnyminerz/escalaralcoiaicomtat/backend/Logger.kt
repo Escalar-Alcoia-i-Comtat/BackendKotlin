36902ff892d5b737b531354cd13a93c958bec73a
Arnau Mora <arnyminerz@proton.me>
2023-08-05 18:20:38 +0200

    Added `debug`
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/Logger.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/Logger.kt
@@ -3,6 +3,8 @@ package com.arnyminerz.escalaralcoiaicomtat.backend
 import com.arnyminerz.escalaralcoiaicomtat.backend.utils.AnsiColors
 
 object Logger {
+    fun debug(message: String) = println(AnsiColors.WHITE + message + AnsiColors.RESET)
+
     fun info(message: String) = println(AnsiColors.BLUE + message + AnsiColors.RESET)
 
     fun warn(message: String) = println(AnsiColors.YELLOW + message + AnsiColors.RESET)
