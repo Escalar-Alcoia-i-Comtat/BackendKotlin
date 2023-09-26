70cc1d5c9db719cfd961c67a282655f0007f7e7a
Arnau Mora <arnyminerz@proton.me>
2023-09-26 11:50:49 +0200

    Fixed constructors
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/InfoEntry.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/InfoEntry.kt
@@ -4,6 +4,8 @@ import com.arnyminerz.escalaralcoiaicomtat.backend.database.table.InfoTable
 import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.id.EntityID
 
-abstract class InfoEntry(id: String): Entity<String>(EntityID(id = id, table = InfoTable)) {
+abstract class InfoEntry(id: EntityID<String>): Entity<String>(id) {
+    constructor(id: String): this(EntityID(id = id, table = InfoTable))
+
     var value by InfoTable.value
 }
--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/LastUpdate.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/LastUpdate.kt
@@ -3,21 +3,23 @@ package com.arnyminerz.escalaralcoiaicomtat.backend.database.entity.info
 import com.arnyminerz.escalaralcoiaicomtat.backend.database.table.InfoTable
 import java.time.Instant
 import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 
-class LastUpdate: InfoEntry(ID) {
+class LastUpdate(id: EntityID<String>): InfoEntry(id) {
+
     companion object: EntityClass<String, LastUpdate>(InfoTable) {
-        const val ID = "last_update"
+        private const val ID = "last_update"
 
         fun get(): Instant? = findById(ID)?.value?.toLong()?.let(Instant::ofEpochMilli)
 
         fun set(timestamp: Instant = Instant.now()) {
-            val value = findById(ID)
-            if (value != null) {
+            val entry = findById(ID)
+            if (entry != null) {
                 // Already exists, update
-                value.value = timestamp.toEpochMilli().toString()
+                entry.value = timestamp.toEpochMilli().toString()
             } else {
-                LastUpdate.new {
-                    this.value = timestamp.toEpochMilli().toString()
+                LastUpdate.new(ID) {
+                    value = timestamp.toEpochMilli().toString()
                 }
             }
         }
