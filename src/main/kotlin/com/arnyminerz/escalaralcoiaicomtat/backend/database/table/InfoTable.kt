a2e865d596774ef72be633a704d0d47fd844a8b2
Arnau Mora <arnyminerz@proton.me>
2023-09-26 12:04:46 +0200

    Replaced magic numbers
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/table/InfoTable.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/table/InfoTable.kt
@@ -1,12 +1,14 @@
 package com.arnyminerz.escalaralcoiaicomtat.backend.database.table
 
+import com.arnyminerz.escalaralcoiaicomtat.backend.database.SqlConsts.INFO_ID_LENGTH
+import com.arnyminerz.escalaralcoiaicomtat.backend.database.SqlConsts.INFO_VALUE_LENGTH
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.Column
 
 object InfoTable: IdTable<String>() {
-    override val id: Column<EntityID<String>> = varchar("id", 16).entityId()
-    val value: Column<String> = varchar("value", 128)
+    override val id: Column<EntityID<String>> = varchar("id", INFO_ID_LENGTH).entityId()
+    val value: Column<String> = varchar("value", INFO_VALUE_LENGTH)
 
     override val primaryKey: PrimaryKey = PrimaryKey(id, name = "PK_InfoTable_Name")
 }
