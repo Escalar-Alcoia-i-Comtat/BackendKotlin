1b17278fad83c801d420747a6d27417f17dacda9
Arnau Mora <arnyminerz@proton.me>
2023-09-29 18:50:47 +0200

    Added Transaction context
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/InfoEntryCompanion.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/InfoEntryCompanion.kt
@@ -1,7 +1,11 @@
 package com.arnyminerz.escalaralcoiaicomtat.backend.database.entity.info
 
+import org.jetbrains.exposed.sql.Transaction
+
 interface InfoEntryCompanion<Type: Any> {
+    context(Transaction)
     fun get(): Type?
 
+    context(Transaction)
     fun update(value: Type)
 }
--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/LastUpdate.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/entity/info/LastUpdate.kt
@@ -4,14 +4,17 @@ import com.arnyminerz.escalaralcoiaicomtat.backend.database.table.InfoTable
 import java.time.Instant
 import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.Transaction
 
 class LastUpdate(id: EntityID<String>): InfoEntry(id) {
 
     companion object: EntityClass<String, LastUpdate>(InfoTable), InfoEntryCompanion<Instant> {
         private const val ID = "last_update"
 
+        context(Transaction)
         override fun get(): Instant? = findById(ID)?.value?.toLong()?.let(Instant::ofEpochMilli)
 
+        context(Transaction)
         override fun update(value: Instant) {
             val entry = findById(ID)
             if (entry != null) {
@@ -24,6 +27,7 @@ class LastUpdate(id: EntityID<String>): InfoEntry(id) {
             }
         }
 
+        context(Transaction)
         fun set(value: Instant = Instant.now()) = update(value)
     }
 }
