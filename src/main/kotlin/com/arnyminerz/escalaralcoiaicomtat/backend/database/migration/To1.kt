8b579a2cd1530662ff4cc082fb1748f831f1497b
Arnau Mora <arnyminerz@proton.me>
2023-09-29 19:27:38 +0200

    Removed catch content
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/migration/To1.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/migration/To1.kt
@@ -1,6 +1,5 @@
 package com.arnyminerz.escalaralcoiaicomtat.backend.database.migration
 
-import com.arnyminerz.escalaralcoiaicomtat.backend.Logger
 import com.arnyminerz.escalaralcoiaicomtat.backend.database.SqlConsts
 import com.arnyminerz.escalaralcoiaicomtat.backend.database.table.Paths
 import org.jetbrains.exposed.sql.Transaction
@@ -10,12 +9,7 @@ object To1 : Migration(null, 1) {
     override suspend fun Transaction.migrate() {
         try {
             exec("ALTER TABLE Paths ADD images varchar(${SqlConsts.FILE_LENGTH * Paths.MAX_IMAGES});")
-        } catch (e: PSQLException) {
-            if (e.message?.contains("ERROR: column \"images\" of relation \"paths\" already exists") == true) {
-                Logger.debug("Initial migration already has been run.")
-            } else {
-                throw e
-            }
+        } catch (_: PSQLException) {
         }
     }
 }
