e727c8b5c3076a551eb63abd8697b2508f2524c1
Arnau Mora <arnyminerz@proton.me>
2023-09-29 19:08:18 +0200

    Added more degrees
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/data/Grade.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/data/Grade.kt
@@ -19,9 +19,9 @@ interface GradeValue {
 enum class SportsGrade : GradeValue {
     G1,
     G2, G2_PLUS,
-    G3A, G3B, G3C,
-    G4A, G4B, G4C,
-    G5A, G5B, G5C, G5_PLUS,
+    G3A, G3B, G3C, G3,
+    G4A, G4B, G4C, G4,
+    G5A, G5B, G5C, G5_PLUS, G5,
     G6A, G6A_PLUS, G6B, G6B_PLUS, G6C, G6C_PLUS,
     G7A, G7A_PLUS, G7B, G7B_PLUS, G7C, G7C_PLUS,
     G8A, G8A_PLUS, G8B, G8B_PLUS, G8C, G8C_PLUS,
