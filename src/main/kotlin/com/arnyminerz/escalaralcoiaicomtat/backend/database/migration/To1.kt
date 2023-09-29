f48bda83edab3f9830f5e2aa8d95af517b1b01b4
Arnau Mora <arnyminerz@proton.me>
2023-09-29 19:33:09 +0200

    Changed catch exception
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/migration/To1.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/database/migration/To1.kt
@@ -2,14 +2,14 @@ package com.arnyminerz.escalaralcoiaicomtat.backend.database.migration
 
 import com.arnyminerz.escalaralcoiaicomtat.backend.database.SqlConsts
 import com.arnyminerz.escalaralcoiaicomtat.backend.database.table.Paths
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.Transaction
-import org.postgresql.util.PSQLException
 
 object To1 : Migration(null, 1) {
     override suspend fun Transaction.migrate() {
         try {
             exec("ALTER TABLE Paths ADD images varchar(${SqlConsts.FILE_LENGTH * Paths.MAX_IMAGES});")
-        } catch (_: PSQLException) {
+        } catch (_: ExposedSQLException) {
         }
     }
 }
