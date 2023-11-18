73a3b6237ba51ebae1d6d504d1e99e2dc609ade8
Arnau Mora <arnyminerz@proton.me>
2023-11-18 10:00:05 +0100

    Improved execution
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/diagnostics/Performance.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/diagnostics/Performance.kt
@@ -6,14 +6,25 @@ import io.sentry.SpanStatus
 object Performance {
     /**
      * Uses the Sentry SDK to measure the performance on the operation inside [block].
+     * If [Diagnostics] is not initialized, [block] is still run, but no measurements will be made.
      *
      * @param name The transaction name
      * @param operation The operation being performed
+     * @param block The block of code to be executed.
      *
-     * @return The result of [block], or `null` if [Diagnostics] is not initialized.
+     * @return The result of [block].
      */
-    inline fun <Result> measure(name: String, operation: String, block: PerformanceScope.() -> Result): Result? {
-        if (!Diagnostics.isInitialized) return null
+    inline fun <Result> measure(name: String, operation: String, block: PerformanceScope.() -> Result): Result {
+        if (!Diagnostics.isInitialized) {
+            return block(
+                object : PerformanceScope(null) {
+                    override fun <ResultType> span(
+                        operation: String,
+                        block: PerformanceScope.() -> ResultType
+                    ): ResultType = block()
+                }
+            )
+        }
 
         val transaction = Sentry.startTransaction(name, operation)
         return try {
@@ -23,12 +34,12 @@ object Performance {
                         operation: String,
                         block: PerformanceScope.() -> ResultType
                     ): ResultType {
-                        val span = this.transaction.startChild(operation)
+                        val span = this.transaction!!.startChild(operation)
                         return try {
                             block()
-                        } catch (exception: Exception) {
-                            transaction.throwable = exception
-                            transaction.status = SpanStatus.INTERNAL_ERROR
+                        } catch (@Suppress("TooGenericExceptionCaught") exception: Exception) {
+                            span.throwable = exception
+                            span.status = SpanStatus.INTERNAL_ERROR
                             throw exception
                         } finally {
                             span.finish()
--- src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/diagnostics/PerformanceScope.kt
+++ src/main/kotlin/com/arnyminerz/escalaralcoiaicomtat/backend/diagnostics/PerformanceScope.kt
@@ -2,6 +2,6 @@ package com.arnyminerz.escalaralcoiaicomtat.backend.diagnostics
 
 import io.sentry.ITransaction
 
-abstract class PerformanceScope(protected val transaction: ITransaction) {
+abstract class PerformanceScope(protected val transaction: ITransaction?) {
     abstract fun <ResultType> span(operation: String, block: PerformanceScope.() -> ResultType): ResultType
 }
