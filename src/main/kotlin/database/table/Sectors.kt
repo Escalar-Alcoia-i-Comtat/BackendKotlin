5f4c1d679bd65278df4c83d0f5618c2ec3ecdb91
Arnau Mora <arnyminerz@proton.me>
2024-04-17 11:13:45 +0200

    Added GPX file
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/database/table/Sectors.kt
+++ src/main/kotlin/database/table/Sectors.kt
@@ -7,6 +7,7 @@ object Sectors : BaseTable() {
     val displayName = varchar("display_name", SqlConsts.DISPLAY_NAME_LENGTH)
 
     val imagePath = varchar("image", SqlConsts.FILE_LENGTH)
+    val gpxPath = varchar("gpx", SqlConsts.FILE_LENGTH).nullable()
 
     val latitude = double("latitude").nullable()
     val longitude = double("longitude").nullable()
