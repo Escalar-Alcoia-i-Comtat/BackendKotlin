5cd2abd9d4d95102c5f50312b80bd5242e78050c
Arnau Mora <arnyminerz@proton.me>
2024-04-18 11:58:58 +0200

    Moved common functions to file
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ src/test/kotlin/server/base/Common.kt
@@ -0,0 +1,60 @@
+package server.base
+
+import database.EntityTypes
+import database.entity.Area
+import database.entity.BaseEntity
+import database.entity.Blocking
+import database.entity.Path
+import database.entity.Sector
+import database.entity.Zone
+import io.ktor.server.testing.ApplicationTestBuilder
+import kotlin.test.assertNotNull
+import server.DataProvider
+
+context(ApplicationTestBuilder)
+suspend fun EntityTypes<*>.provide(): Int? {
+    when (this) {
+        EntityTypes.AREA -> {
+            return DataProvider.provideSampleArea()
+        }
+        EntityTypes.ZONE -> {
+            val areaId = DataProvider.provideSampleArea()
+            assertNotNull(areaId)
+            return DataProvider.provideSampleZone(areaId)
+        }
+        EntityTypes.SECTOR -> {
+            val areaId = DataProvider.provideSampleArea()
+            assertNotNull(areaId)
+
+            val zoneId = DataProvider.provideSampleZone(areaId)
+            assertNotNull(zoneId)
+
+            return DataProvider.provideSampleSector(zoneId)
+        }
+        EntityTypes.PATH -> {
+            val areaId = DataProvider.provideSampleArea()
+            assertNotNull(areaId)
+
+            val zoneId = DataProvider.provideSampleZone(areaId)
+            assertNotNull(zoneId)
+
+            val sectorId = DataProvider.provideSampleSector(zoneId)
+            assertNotNull(sectorId)
+
+            return DataProvider.provideSamplePath(sectorId)
+        }
+        else -> error("Not implemented")
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <Type: BaseEntity> EntityTypes<Type>.getter(id: Int): Type {
+    return when (this) {
+        EntityTypes.AREA -> Area[id] as Type
+        EntityTypes.ZONE -> Zone[id] as Type
+        EntityTypes.SECTOR -> Sector[id] as Type
+        EntityTypes.PATH -> Path[id] as Type
+        EntityTypes.BLOCKING -> Blocking[id] as Type
+        else -> error("Not implemented")
+    }
+}
