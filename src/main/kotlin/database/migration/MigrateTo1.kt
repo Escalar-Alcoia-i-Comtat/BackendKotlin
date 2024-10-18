f3f9bb621d6b85304f586e8a4ea667ba2342c411
Arnau Mora <arnyminerz@proton.me>
2024-10-18 21:39:09 +0200

    Fixed migrations
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- src/main/kotlin/database/migration/MigrateTo1.kt
+++ src/main/kotlin/database/migration/MigrateTo1.kt
@@ -1,6 +1,5 @@
 package database.migration
 
-import data.DataPoint
 import data.LatLng
 import database.entity.Zone
 import database.serialization.Json
@@ -13,15 +12,30 @@ object MigrateTo1 : Migration(null, 1) {
         migrateZones()
     }
 
-    private fun migrateZones() {
+    private fun Transaction.migrateZones() {
         Zone.all().forEach { zone ->
+            // Move from latitude and longitude to point
             val lat = zone.latitude
             val lon = zone.longitude
             if (lat != null && lon != null) zone.point = LatLng(lat, lon)
+            zone.latitude = null
+            zone.longitude = null
 
-            zone.pointsString?.let { points ->
-                zone.points = Json.decodeFromString<List<DataPoint>>(points)
+            // Move from pointsString to points
+            val pointsString = zone.pointsString
+            if (pointsString == "\u0000") {
+                zone.points = emptyList()
+            } else if (!pointsString.isNullOrEmpty()) {
+                // Points were saved sometimes in a wrong JSON format. Instead of arrays, they were objects in multiple lines.
+                if (pointsString.startsWith("{")) {
+                    pointsString.split('\n').joinToString(",") { it.trim() }.let {
+                        zone.points = Json.decodeFromString("[$it]")
+                    }
+                } else {
+                    zone.points = Json.decodeFromString(pointsString)
+                }
             }
+            zone.pointsString = null
         }
     }
 }
