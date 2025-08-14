889592307ddc752c2948fbbb0eb062af5f9bc16e
Arnau Mora <arnyminerz@proton.me>
2025-08-14 18:19:52 +0200

    Add description to points (#252)

--- src/main/kotlin/data/DataPoint.kt
+++ src/main/kotlin/data/DataPoint.kt
@@ -10,12 +10,14 @@ import kotlinx.serialization.encodeToString
  * @property location The location of the data point.
  * @property label The label associated with the data point.
  * @property icon The icon associated with the data point.
+ * @property description An extra optional text to explain more about the point.
  */
 @Serializable
 data class DataPoint(
     val location: LatLng,
     val label: String,
-    val icon: String
+    val icon: String,
+    val description: String? = null,
 ) {
     override fun toString(): String = Json.encodeToString(this)
 }
