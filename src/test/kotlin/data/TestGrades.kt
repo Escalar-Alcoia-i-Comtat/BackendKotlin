d7071d88173f6807cef13d1d4e206fdbbea30f3e
Arnau Mora <arnyminerz@proton.me>
2025-11-10 18:45:44 +0100

    Change assert message

--- src/test/kotlin/data/TestGrades.kt
+++ src/test/kotlin/data/TestGrades.kt
@@ -21,7 +21,11 @@ class TestGrades {
     @Test
     fun `test fromString name`() {
         for (grade in Grade.entries) {
-            assertEquals(grade, Grade.fromString(grade.name))
+            assertEquals(
+                grade,
+                Grade.fromString(grade.name),
+                "Conversion failed for name \"${grade.name}\" => $grade",
+            )
         }
     }
 }
